6f8af9980fe36fb4996f50cacf96a559504f6a7a
ETO <161434685+amadeus2486@users.noreply.github.com>
2025-02-15 21:09:17 +0800

    Add files via upload

---
+++ Windows基础(C#)/MusicPlayer/music2/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace music2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 36;
+            this.listBox1.Location = new System.Drawing.Point(462, 136);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(297, 400);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(371, 556);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "正在播放：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(44, 429);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "音量调控";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.trackBar1.Location = new System.Drawing.Point(49, 192);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar1.Size = new System.Drawing.Size(69, 200);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(121, 411);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(667, 46);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(174, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 51);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "设置音乐播放列表";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(222, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "停止音乐";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(222, 209);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 40);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "暂停音乐";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(222, 352);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 40);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "下一首";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(222, 286);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 40);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "播放音乐";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button6.Location = new System.Drawing.Point(829, 40);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(239, 51);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "点播OGG音乐";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.Location = new System.Drawing.Point(880, 136);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(144, 40);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "停止音乐";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button8.Location = new System.Drawing.Point(880, 209);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(144, 40);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "暂停音乐";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button9.Location = new System.Drawing.Point(880, 286);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(144, 40);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "播放音乐";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button10.Location = new System.Drawing.Point(1130, 1);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(138, 50);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "设置背景";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(1092, 429);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 28);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "声音开关";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.trackBar2.Location = new System.Drawing.Point(1119, 192);
+            this.trackBar2.Maximum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar2.Size = new System.Drawing.Size(69, 200);
+            this.trackBar2.TabIndex = 15;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1329, 837);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "正在播放：";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Windows基础(C#)/MusicPlayer/music2/Form1.cs
@@ -0,0 +1,197 @@
+﻿using NAudio.Vorbis;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace music2
+{
+    public partial class Form1 : Form
+    {
+        string[] files;
+        List<string> LocalmusicList = new List<string>();
+        WaveOutEvent waveOut = new WaveOutEvent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void musicplay(string filename)
+        {
+            string extension = Path.GetExtension(filename);
+            if (extension == ".ogg")
+            {
+                Console.WriteLine("this is ogg file");
+            }
+            else
+            {
+                Console.WriteLine("this is not ogg file");
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (LocalmusicList.Count > 0||LocalmusicList==null)
+            {
+                axWindowsMediaPlayer1.URL = LocalmusicList[listBox1.SelectedIndex];
+                if (!File.Exists(axWindowsMediaPlayer1.URL))
+                {
+                    throw new FileNotFoundException("The audio file does not exist.", axWindowsMediaPlayer1.URL);
+                }
+                musicplay(axWindowsMediaPlayer1.URL);
+                label1.Text = Path.GetFileNameWithoutExtension(LocalmusicList[listBox1.SelectedIndex]);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+            axWindowsMediaPlayer1.settings.volume = trackBar1.Value;
+            label2.Text = "音量：" + trackBar1.Value + "%";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "选择音频|*mp3;*.wav;*.flac";
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                waveOut.Stop();
+                waveOut.Volume = 0;
+                listBox1.Items.Clear();
+                if (files != null)
+                {
+                    Array.Clear(files, 0, files.Length);
+                }
+                files = openFileDialog1.FileNames;
+                LocalmusicList.Clear();
+                string[] array = files;
+                foreach (string x in array)
+                {
+                    LocalmusicList.Add(x);
+                    listBox1.Items.Add(Path.GetFileName(x));
+                }
+                //listBox1.Text = openFileDialog1.FileName;   
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (waveOut.PlaybackState == PlaybackState.Playing)
+            {
+
+            }
+            else
+            {
+                int nextIndex = listBox1.SelectedIndex + 1;
+                if (LocalmusicList.Count > 0)
+
+                    if (nextIndex >= LocalmusicList.Count)
+                    {
+                        nextIndex = 0;
+                    }
+                axWindowsMediaPlayer1.URL = LocalmusicList[nextIndex];
+                musicplay(axWindowsMediaPlayer1.URL);
+                label1.Text = Path.GetFileNameWithoutExtension(LocalmusicList[nextIndex]);
+                listBox1.SelectedIndex = nextIndex;
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (waveOut.PlaybackState==PlaybackState.Stopped)
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            else axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+        //
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string oggFilePath = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "播放音频|*.ogg";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.stop();
+                axWindowsMediaPlayer1.close();
+                LocalmusicList.Clear() ;
+                oggFilePath = openFileDialog.FileName;
+                listBox1.Items.Clear();
+                listBox1.Items.Add(Path.GetFileName(oggFilePath));
+                label1.Text = Path.GetFileNameWithoutExtension(Path.GetFileName(oggFilePath));
+                VorbisWaveReader vorbisReader = new VorbisWaveReader(oggFilePath);
+                if(waveOut!=null)
+                    waveOut.Dispose();
+                waveOut.Init(vorbisReader);
+                waveOut.Play();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (waveOut != null)
+            {
+                waveOut.Pause();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (waveOut != null && waveOut.PlaybackState != PlaybackState.Playing && LocalmusicList.Count==0)
+            {
+                waveOut.Play();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string path;
+            openFileDialog1 = new OpenFileDialog();
+            if(openFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                path = openFileDialog1.FileName;
+                BackgroundImage = Image.FromFile(path);
+            }
+            
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            waveOut.Volume = trackBar2.Value;
+            if (trackBar2.Value == 0)
+                label3.Text = "已关闭声音";
+            else label3.Text = "已开启声音";
+        }
+    }
+}
